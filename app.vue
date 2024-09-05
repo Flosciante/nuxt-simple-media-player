1ad4c8d52d035e83dfe6bf56f2a26c2c0a863940
Florent Delerue <florentdelerue@hotmail.com>
2024-09-05 17:33:43 +0200

    feat: list and header (wip)

---
+++ app.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { usePlaylistStore } from '@/stores/usePlaylistStore';
+
+const playlistStore = usePlaylistStore();
+
+await playlistStore.fetchPlaylist();
+</script>
+
+<template>
+  <div>
+    <header class="h-20 border-b sticky top-0 backdrop-blur-sm bg-zinc-900/90 z-50 flex justify-between px-4 items-center gap-x-4 neon">
+      <UInput class="w-full" :ui="{ rounded: 'rounded-full' }" icon="i-heroicons-glass" />
+      <UAvatar src="https://avatars.githubusercontent.com/u/6696789?v=4" />
+
+
+    </header>
+
+    <UContainer>
+      <NuxtPage />
+    </UContainer>
+  </div>
+</template>
+
+<style scoped>
+.neon {
+  border: none;
+  border-bottom: 0.1rem solid #fff;
+  padding-bottom: 0.1em;
+  box-shadow: 0 2px 0.2rem #fff,
+              0 2px 0.2rem #fff,
+              0 2px 2rem #bc13fe,
+              0 2px 0.8rem #bc13fe,
+              0 2px 2.8rem #bc13fe,
+              inset 0 -2px 1.3rem #bc13fe;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { usePlaylistStore } from '@/stores/usePlaylistStore';
+
+const playlistStore = usePlaylistStore()
+const playlist = playlistStore.getPlaylist
+
+const columns = [{
+  key: 'position',
+  label: '#'
+}, {
+  key: 'name',
+  label: 'Title'
+}, {
+  key: 'album_name',
+  label: 'Album'
+}, {
+  key: 'releasedate',
+  label: 'Release date'
+}, {
+  key: 'duration',
+  label: 'Duration'
+}]
+</script>
+
+<template>
+  <div>
+    <UTable :rows="playlist" :columns="columns">
+      <template #name-data="{ row }">
+        <div class="flex flex-col">
+          <span class="text-white">{{ row.name }}</span>
+          <span>{{ row.artist_name }}</span>
+        </div>
+      </template>
+    </UTable>
+  </div>
+</template>
